89ea41540433f79e431fa66f8e83a38a676766ac
lumingcao <lumingcao01@gmail.com>
2024-02-03 21:10:22 -0800

    home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 import SearchBarWithFilter from "../Components/SearchBarWithFilter";
 import "./../styles/Home.css";
 
+
 const Home = () => {
   return (
-    <div>
+    <div >
       <Link
         to="/AllCompaniesPage"
         className="text-blue-500 hover:text-blue-800"
@@ -21,15 +22,23 @@ const Home = () => {
       <Link to="/AddReviewPage" className="text-blue-500 hover:text-blue-800">
         AddReview Page
       </Link>
+      
+      {/*  
       <div className="logo">
         <img src="/Glossdoor.png" alt="Glossdoor"></img>
       </div>
-
-      <div className="Background">
+*/}<div className="home-page">
+       <div className="Background">
         <img src="/home_bg.png"></img>
+        <div className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl font-bold">
+        Glossdoor
+        
+      </div>
+      <SearchBarWithFilter />
+      </div>
       </div>
 
-      <SearchBarWithFilter />
+      
     </div>
   );
 };
