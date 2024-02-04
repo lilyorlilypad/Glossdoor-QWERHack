3989bd079af82f964dccbbbb165eaac7c92695a3
skematt <matthewyang2002@hotmail.com>
2024-02-03 22:43:33 -0800

    UI change for all company page

--- frontend/src/Pages/AllCompaniesPage.jsx
+++ frontend/src/Pages/AllCompaniesPage.jsx
@@ -3,7 +3,6 @@ import CompanyCard from "../Components/CompanyCard";
 import SearchBarWithFilter from "../Components/SearchBarWithFilter";
 import apiConfig from "../apiConfig";
 
-//import "./../styles/AllCompaniesPage.css";
 
 const AllCompaniesPage = () => {
   const [companies, setCompanyData] = useState({});
@@ -27,26 +26,37 @@ const AllCompaniesPage = () => {
       });
   });
   return (
-    <div className="filter-result-page  mt-10">
+    <div className="filter-result-page mt-10">
       <header></header>
-      <SearchBarWithFilter />
-      <div className="px-4 py-2 flex justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {companies.length > 0 ? (
-            companies.map((company) => (
-              <CompanyCard
-                id={company._id} // Ensure you have a unique key for each item
-                title={company.companyName}
-                description={company.companyDescription}
-                rating={company.ratings[0]}
-                logo={`/${company.companyName}.png`}
-                // Add other necessary props
-              />
-            ))
-          ) : (
-            <p className="text-center">No companies found.</p>
-          )}
+        <div className="container bg-primary mx-auto pt-3 rounded-lg">
+            <div className="z-10 text-center">
+                <h1 className={"text-dark text-4xl font-bold mb-5 mt-5"}>Glossdoor</h1>
+                <SearchBarWithFilter className="mt-10"/>
+            </div>
         </div>
+
+
+      <div className="flex container mx-auto justify-center px-2 py-2">
+          <div className=" flex flex-col bg-gray-200 py-6 px-6 items-center rounded-lg">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
+                  {companies.length > 0 ? (
+                      companies.map((company) => (
+                          <CompanyCard
+                              id={company._id} // Ensure you have a unique key for each item
+                              title={company.companyName}
+                              description={company.companyDescription}
+                              rating={company.ratings[0]}
+                              logo={`/${company.companyName}.png`}
+                              // Add other necessary props
+                          />
+                      ))
+                  ) : (
+                      <p className="text-center">No companies found.</p>
+                  )}
+              </div>
+
+          </div>
+
       </div>
     </div>
   );
