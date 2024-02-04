868c93910234f0a1798db5b8174b1aaf4777d0c5
Vincent Lin <vinlin24@outlook.com>
2024-02-04 00:40:07 -0800

    Add some style to the add review page

--- frontend/src/Pages/AddReviewPage.jsx
+++ frontend/src/Pages/AddReviewPage.jsx
@@ -1,30 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import RatingScale from "../Components/RatingScale";
 import "./../styles/AddReviewPage.css";
 const AddReviewPage = () => {
   return (
     <>
       <RatingScale />
-      <div className="review-container">
-        <h2>COMMENT</h2>
+
+      <div className="review-container bg-white p-6 rounded-lg shadow-md my-6 w-full">
+        <h2 className="text-2xl font-semibold mb-4">COMMENT</h2>
         <textarea
+          className="w-full p-2 border border-gray-300 rounded bg-gray-100"
           name="comments"
-          cols="100"
-          rows="10"
+          cols={100}
+          rows={10}
           placeholder="Describe your experience working in this company"
         ></textarea>
       </div>
-      <div className="review-container">
-        <h2>Wishes</h2>
+
+      <div className="review-container bg-white p-6 rounded-lg shadow-md my-6 w-full">
+        <h2 className="text-2xl font-semibold mb-4">WISHES</h2>
         <textarea
-          name="comments"
-          cols="100"
-          rows="10"
+          className="w-full p-2 border border-gray-300 rounded bg-gray-100"
+          name="wishes"
+          cols={100}
+          rows={10}
           placeholder="What can the company improve on to build a more inclusive culture"
         ></textarea>
       </div>
+
       <div className="button-container">
-        <button className="submit-button">submit</button>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+          Submit
+        </button>
       </div>
     </>
   );
