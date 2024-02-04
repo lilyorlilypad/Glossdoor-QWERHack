7bd2bf1c4a0ce5069b3c01f5edfe7e204e57ebc9
skematt <matthewyang2002@hotmail.com>
2024-02-04 00:33:35 -0800

    small ui change on company cards

--- frontend/src/components/CompanyCard.js
+++ frontend/src/components/CompanyCard.js
@@ -8,10 +8,10 @@ const CompanyCard = ({ id, title, logo, rating, description }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4">
       <Link to={`/company/${id}`} className="no-underline text-black">
-        <div className="px-6 py-4">
-          <div className="flex row">
+        <div className="px-4 py-4">
+          <div className="flex row justify-end">
             <div className="font-bold text-xl mb-2">{title}</div>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-0 mb-2">
               Rating: {rating}
             </span>
           </div>
